0cc8fe5750afe373f68a7d933378c3da4a27010b
The Marush <marush.the.first@gmail.com>
2025-03-29 15:59:21 +0100

    remove obsolete stuff from vite example

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 export function toggleSemester(id) {
   const section = document.getElementById(id);
@@ -30,25 +27,3 @@ export function toggleInfo(id) {
 // Attach functions to the global window object
 window.toggleSemester = toggleSemester;
 window.toggleInfo = toggleInfo;
-
-
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
