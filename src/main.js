d86b51131bdc02517ccc9a32e35f6e4ad39c4f5a
The Marush <marush.the.first@gmail.com>
2025-04-11 14:59:12 +0200

    Add art gallery feature with responsive styling

--- src/main.js
+++ src/main.js
@@ -27,3 +27,49 @@ export function toggleInfo(id) {
 // Attach functions to the global window object
 window.toggleSemester = toggleSemester;
 window.toggleInfo = toggleInfo;
+
+const artImages = [
+  {
+    src: "https://i.imgur.com/TVExGvG.jpg",
+    alt: "Artwork 1",
+    caption: "This is a filler caption for artwork 1"
+  },
+  {
+    src: "https://i.imgur.com/Yy0xWQJ.jpg",
+    alt: "Artwork 2",
+    caption: "This is a filler caption for artwork 2"
+  },
+  {
+    src: "https://i.imgur.com/kLsd3wf.jpg",
+    alt: "Artwork 3",
+    caption: "This is a filler caption for artwork 3"
+  }
+];
+
+function generateArtGallery(images) {
+  const container = document.getElementById("myart");
+
+  const gallery = document.createElement("div");
+  gallery.className = "gallery";
+
+  images.forEach(img => {
+    const wrapper = document.createElement("div");
+    wrapper.className = "art-item";
+
+    const image = document.createElement("img");
+    image.src = img.src;
+    image.alt = img.alt;
+
+    const caption = document.createElement("p");
+    caption.textContent = img.caption;
+
+    wrapper.appendChild(image);
+    wrapper.appendChild(caption);
+    gallery.appendChild(wrapper);
+  });
+
+  container.appendChild(gallery);
+}
+
+generateArtGallery(artImages);
+
