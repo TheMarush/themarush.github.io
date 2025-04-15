d39cf59d85531ef4ad302155807f0ab4cc46d4b7
The Marush <marush.the.first@gmail.com>
2025-04-15 13:39:09 +0200

    Update SVG height

--- src/study/semester2.js
+++ src/study/semester2.js
@@ -39,7 +39,7 @@ export function renderSemester2() {
 
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute("width", "100%");
-  svg.setAttribute("height", "800");
+  svg.setAttribute("height", "auto");
   svg.setAttribute("viewBox", "-50 0 1100 800");
   svg.classList.add("rainbow-path");
 
